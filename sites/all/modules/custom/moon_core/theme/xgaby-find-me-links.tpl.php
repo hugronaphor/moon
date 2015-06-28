801d07421ea8dff4f860bf77654a8d4e5c13e2c4
Cornel Andreev <hugronaphor@gmail.com>
2015-06-28 23:47:16 +0100

    Photo page.

---
+++ sites/all/modules/custom/moon_core/theme/xgaby-find-me-links.tpl.php
@@ -0,0 +1,5 @@
+<?php foreach ($links as $title => $link): ?>
+  <div class="find-me-source">
+    <a class="find-me-link" href="<?php print $link; ?>" target="_blank"><?php print $title; ?></a>
+  </div>
+<?php endforeach; ?>
