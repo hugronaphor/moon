42e3330dad86cb8bc589927806999b19ac84522d
Cornel Andreev <hugronaphor@gmail.com>
2015-06-29 13:54:16 +0100

    Add Footer blocks.

---
+++ sites/all/modules/custom/moon_core/theme/moon-social.tpl.php
@@ -0,0 +1,15 @@
+<?php if ($social_count) : ?>
+  <div class="social-buttons">
+    <ul class="footer-social">
+      <?php if ($fb_site_url) : ?>
+        <li rel="fb"><a title="Facebook" href="<?php print $fb_site_url; ?>" target="_blank">Facebook</a></li>
+      <?php endif; ?>
+      <?php if ($twitter_site_url) : ?>
+        <li rel="twitter"><a title="Twitter" href="<?php print $twitter_site_url; ?>" target="_blank">Twitter</a></li>
+      <?php endif; ?>
+      <?php if ($instagram_site_url) : ?>
+        <li rel="instagram"><a title="Instagram" href="<?php print $instagram_site_url; ?>" target="_blank">Instagram</a></li>
+      <?php endif; ?>
+    </ul>
+  </div>
+<?php endif; ?>
