54a0b2897a6820461f7d3d7ed1a4179330660d68
Cornel Andreev <hugronaphor@gmail.com>
2015-07-06 21:24:57 +0100

    Add headline data in photo teaser overlay.

--- sites/all/themes/moon/templates/node/node--photo--teaser.tpl.php
+++ sites/all/themes/moon/templates/node/node--photo--teaser.tpl.php
@@ -8,21 +8,21 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  
-  
+
   <div class="changeme1">
     <?php print render($content['field_image_multiple']); ?>
   </div>
-  
+
   <div class="hidden changeme2">
-    <?php print $title; ?>
+    <span class="teaser-title"><?php print $title; ?></span>
+    <?php print render($content['field_headline']); ?>
   </div>
-  
+
   <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    //print render($content);
+  // We hide the comments and links now so that we can render them later.
+  hide($content['comments']);
+  hide($content['links']);
+  //print render($content);
   ?>
-  
+
 </article>
