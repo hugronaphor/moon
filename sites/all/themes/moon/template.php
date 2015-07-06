c5bf14e3c5a909ba7e80bc4a205259ce0359f263
Cornel Andreev <hugronaphor@gmail.com>
2015-07-06 22:04:46 +0100

    Add placeholders on contact form.

--- sites/all/themes/moon/template.php
+++ sites/all/themes/moon/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Write your theme logic here.
@@ -11,4 +12,20 @@ function moon_preprocess_node(&$vars) {
   if ($vars['view_mode'] == 'teaser') {
     $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__teaser';
   }
+}
+
+function moon_form_contact_site_form_alter(&$form, &$form_state, $form_id) {
+  $form['name']['#attributes']['placeholder'] = t("NAME");
+  $form['name']['#title_display'] = 'invisible';
+
+  $form['mail']['#attributes']['placeholder'] = t("EMAIL");
+  $form['mail']['#title_display'] = 'invisible';
+
+  $form['subject']['#attributes']['placeholder'] = t("SUBJECT");
+  $form['subject']['#title_display'] = 'invisible';
+
+  $form['message']['#attributes']['placeholder'] = t("YURE MESSAGE");
+  $form['message']['#title_display'] = 'invisible';
+
+  unset($form['copy']);
 }
