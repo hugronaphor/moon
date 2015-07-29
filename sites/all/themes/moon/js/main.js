2fd9c87c05e7a46aade0d3b2d3a16891a1e0b508
Cristina <grecucristinai@gmail.com>
2015-07-29 11:27:39 +0100

    hover photo fixed

--- sites/all/themes/moon/js/main.js
+++ sites/all/themes/moon/js/main.js
@@ -1,14 +1,26 @@
 (function ($) {
 
-  'use strict';
+    'use strict';
 
-  /**
-   * A simple Drupal behavior example.
-   */
-  Drupal.behaviors.cirlceThemeExample = {
-    attach: function (context) {
-      // Your code goes here.
-    }
-  };
+    /**
+     * A simple Drupal behavior example.
+     */
+    Drupal.behaviors.cirlceThemeExample = {
+        attach: function (context) {
+            // Your code goes here.
+            //hover for photos//
+            $ = jQuery;
+            $(document).ready(function () {
+                $('.view-display-id-photo_main_all article').hover(
+                    function () {
+                        $(".changeme2", this).fadeIn()
+                    },
+                    function () {
+                        $(".changeme2", this).fadeOut()
+                    });
+            });
+
+        }
+    };
 
 })(jQuery);
