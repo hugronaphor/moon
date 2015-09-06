4f35a4163f4dec9aa065fbe2c8ade50d3a73a3a2
Cornel Andreev <hugronaphor@gmail.com>
2015-09-06 19:16:35 +0100

    Css Tweaks.

--- sites/all/themes/moon/js/main.js
+++ sites/all/themes/moon/js/main.js
@@ -8,14 +8,36 @@
     Drupal.behaviors.photoHover = {
         attach: function (context) {
 
-            $('.view-display-id-photo_main_all a.article').hover(
+            $('.view-display-id-photo_main_all a.article, .view-display-id-photo_main a.article').hover(
                 function () {
-                    $(this).find(".after-hover-content").fadeIn(100);
+                    $(this).find(".on-hover-content").fadeIn(100);
                 },
                 function () {
-                    $(this).find(".after-hover-content").stop().fadeOut(100);
+                    $(this).find(".on-hover-content").stop().fadeOut(100);
                 });
+        }
+    };
 
+    /**
+     * Scroll to top button.
+     */
+    Drupal.behaviors.scrollToTop = {
+        attach: function (context) {
+
+            // Check to see if the window is top if not then display button.
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 500) {
+                    $('.scrollToTop').fadeIn();
+                } else {
+                    $('.scrollToTop').fadeOut();
+                }
+            });
+
+            // Click event to scroll to top.
+            $('.scrollToTop').click(function () {
+                $('html, body').animate({scrollTop: 0}, 400);
+                return false;
+            });
         }
     };
 
--- sites/all/themes/moon/templates/node/node--photo--teaser.tpl.php
+++ sites/all/themes/moon/templates/node/node--photo--teaser.tpl.php
@@ -6,25 +6,22 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728164
  */
-#dsm(get_defined_vars());
 ?>
 <a href="<?php print $node_url; ?>" class="article node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
+  <div class="initial-content">
+    <?php print render($content['field_image_multiple']); ?>
+  </div>
 
-    <div class="initial-content">
-      <?php print render($content['field_image_multiple']); ?>
-    </div>
+  <div class="hidden on-hover-content">
+    <span class="teaser-title"><?php print $title; ?></span>
+    <?php print render($content['field_headline']); ?>
+  </div>
 
-    <div class="hidden after-hover-content">
-      <span class="teaser-title"><?php print $title; ?></span>
-      <?php print render($content['field_headline']); ?>
-    </div>
-
-
-    <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    //print render($content);
-    ?>
-  </a>
+  <?php
+  // We hide the comments and links now so that we can render them later.
+  hide($content['comments']);
+  hide($content['links']);
+  //print render($content);
+  ?>
+</a>
