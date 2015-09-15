882a5ec7f5e7b18c921f1cde357509a2337d8816
Cristina <grecucristinai@gmail.com>
2015-09-15 13:18:16 +0100

    menu fixed

--- sites/all/themes/moon/js/main.js
+++ sites/all/themes/moon/js/main.js
@@ -1,33 +1,6 @@
 (function ($) {
 
-<<<<<<< HEAD
-    'use strict';
-
-    /**
-     * A simple Drupal behavior example.
-     */
-    Drupal.behaviors.cirlceThemeExample = {
-        attach: function (context) {
-            //hover for photos//
-            $ = jQuery;
-            $(document).ready(function () {
-                $('.view-display-id-photo_main_all article').hover(
-                    function () {
-                        if (!$(this).hasClass('animating')) {
-                            var hovered = this;
-                            $(this).addClass('animating');
-                            $(".changeme2", this).fadeIn('slow', function() {
-                                $(hovered).removeClass('animating');
-                            });
-                        }
-                    },
-                    function () {
-                        $(".changeme2", this).fadeOut()
-                    });
-            });
-=======
   'use strict';
->>>>>>> 1658fdfc4fae7dddc961b2e494d46058d7eef017
 
   /**
    * Photo hover behaviour.
